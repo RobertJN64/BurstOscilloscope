1311308add0d123a701acca1a396c7dd176e0963
RobertJN64 <robertjnies@gmail.com>
2022-12-25 22:30:58 -0500

    configurable multichannel oscilloscope

--- StandardOscilloscope/StandardOscilloscope.ino
+++ StandardOscilloscope/StandardOscilloscope.ino
@@ -3,35 +3,44 @@
 #define ch2 A2
 #define ch3 A3
 
+#define CH_COUNT 2
 #define SAMPLE_COUNT 400
-#define SAMPLE_DELAY 10 //ms
+#define SAMPLE_DELAY 1000 //micro seconds
 #define PRESAMPLE_COUNT 50
+#define TRIG false
+#define THRESH 100
+#define RISING_EDGE true //use while(analogRead(ch0) < THRESH)
 
 #define MAX 1000
 #define MIN -200
 
 void presample_log(int value) {
   for (int i = 0; i < PRESAMPLE_COUNT/2; i++) {
-    Serial.print("CH_0:");
-    analogRead(ch0);
-    Serial.print(value);
-    Serial.print(",");
-
-    Serial.print("CH_1:");
-    analogRead(ch1);
-    Serial.print(value);
-    Serial.print(",");
-
-    Serial.print("CH_2:");
-    analogRead(ch2);
-    Serial.print(value);
-    Serial.print(",");
-
-    Serial.print("CH_3:");
-    analogRead(ch3);
-    Serial.print(value);
+    if (CH_COUNT > 0) {
+      Serial.print("CH_0:");
+      analogRead(ch0);
+      Serial.print(value);
+    }
+    if (CH_COUNT > 1) {
+      Serial.print(",");
+      Serial.print("CH_1:");
+      analogRead(ch1);
+      Serial.print(value);
+    }
+    if (CH_COUNT > 2) {
+      Serial.print(",");
+      Serial.print("CH_2:");
+      analogRead(ch2);
+      Serial.print(value);
+    }
+    if (CH_COUNT > 3) {
+      Serial.print(",");
+      Serial.print("CH_3:");
+      analogRead(ch3);
+      Serial.print(value);
+    }
     Serial.println();
-    delay(SAMPLE_DELAY);
+    delayMicroseconds(SAMPLE_DELAY);
   }
 }
 
@@ -45,23 +54,34 @@ void setup() {
   presample_log(MAX);
   presample_log(MIN);
 
+  if (TRIG) {
+    while(((analogRead(ch0) < THRESH) && RISING_EDGE) || ((analogRead(ch0) > THRESH) && !RISING_EDGE)) {
+      
+    }
+  }
+
   for (int i = 0; i < SAMPLE_COUNT; i++) {
-    Serial.print("CH_0:");
-    Serial.print(analogRead(ch0));
-    Serial.print(",");
-  
-    Serial.print("CH_1:");
-    Serial.print(analogRead(ch1));
-    Serial.print(",");
-  
-    Serial.print("CH_2:");
-    Serial.print(analogRead(ch2));
-    Serial.print(",");
-  
-    Serial.print("CH_3:");
-    Serial.print(analogRead(ch3));
+    if (CH_COUNT > 0) {
+      Serial.print("CH_0:");
+      Serial.print(analogRead(ch0));
+    }
+    if (CH_COUNT > 1) {
+      Serial.print(",");
+      Serial.print("CH_1:");
+      Serial.print(analogRead(ch1));
+    }
+    if (CH_COUNT > 2) {
+      Serial.print(",");
+      Serial.print("CH_2:");
+      Serial.print(analogRead(ch2));
+    }
+    if (CH_COUNT > 3) {
+      Serial.print(",");
+      Serial.print("CH_3:");
+      Serial.print(analogRead(ch3));
+    }
     Serial.println();
-    delay(SAMPLE_DELAY);
+    delayMicroseconds(SAMPLE_DELAY);
   }
 }
 
